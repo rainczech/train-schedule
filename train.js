b61486563d917e17d1b73ce992ef158469ca781c
Grace Ho <rainczech@gmail.com>
2018-03-18 11:02:34 -0500

    css and firebase added

---
+++ train.js
@@ -0,0 +1,9 @@
+var config = {
+    apiKey: "AIzaSyC1ODg4uxCOFhVvKa2dp1ctiZgkRW5WlmA",
+    authDomain: "train-schedule-7ffc1.firebaseapp.com",
+    databaseURL: "https://train-schedule-7ffc1.firebaseio.com",
+    projectId: "train-schedule-7ffc1",
+    storageBucket: "train-schedule-7ffc1.appspot.com",
+    messagingSenderId: "967691471378"
+  };
+  firebase.initializeApp(config);
